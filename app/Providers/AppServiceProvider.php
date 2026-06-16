24e905977de8c67765c64564f2171e9d3361d6a4
erihidayat06 <erihidayat549@gmial.com>
2026-06-16 08:04:10 +0700

    Memperbaiki Ekosistem

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Models\Proposal;
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,7 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::policy(Proposal::class, null);
+        //
     }
 
     // protected $policies = [
